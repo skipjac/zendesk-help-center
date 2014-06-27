cab32e90f18d2dae16c9c602492cbb7e4e492d78
Skip Moore <skip@skipjack.info>
2014-06-27 10:24:07 -0700

    added comments to make clear what the functions do

--- set-default-dropdown-value.js
+++ set-default-dropdown-value.js
@@ -1,3 +1,5 @@
+
+//walk the ticket field data object
 function traverse(obj,func, parent) {
   for (i in obj){
     func.apply(this,[i,obj[i],parent]);      
@@ -6,7 +8,7 @@ function traverse(obj,func, parent) {
     }
   }
 }
- 
+//traverse the ticket field data obj and flatten it if nested 
 function getPropertyRecursive(obj, property){
   var acc = [];
   traverse(obj, function(key, value, parent){
@@ -16,6 +18,7 @@ function getPropertyRecursive(obj, property){
   });
   return acc;
 }
+//set the defatul value
 function setDefaultVaule(field, setDefault){
    $('.request_custom_fields_' + field).one('DOMNodeInserted', function(d){
      //console.log($(d.currentTarget).children('input').data('tagger'));
@@ -30,4 +33,13 @@ function setDefaultVaule(field, setDefault){
      $(d.target).text(skip[0].parent);
    });
   }
+//build a list of ticket field ID's and their defalut tag value
 var listOfDefaults = { '22103126' : 'tree__nest_in__bird_type', '21631456': 'dog'};
+//wait until things are loaded and run the function
+$(document).ready(function(){
+ 
+for( key in listOfDefaults){
+  console.log(key + ' ' + listOfDefaults[key]);
+  setDefaultVaule(key, listOfDefaults[key]);
+}
+})
